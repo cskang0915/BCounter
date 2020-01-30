d116f93acf46dba112e984ea9801c1337969acc6
Nicolas Hill <nrhill1@gmail.com>
2020-01-29 17:48:51 -0800

    worked on authentication, starting on user

--- backend/middleware/authRequired.js
+++ backend/middleware/authRequired.js
@@ -6,7 +6,34 @@ module.exports = (req, res, next) => {
 	const bearerHeader = req.headers["authorization"]
 
 	if(typeof bearerHeader !== 'undefined') {
-		const bearer = bearerHeader.split(" ")
-		const bearerToken = bearer[1]
-	}
-}
+		const bearer = bearerHeader.split(" "); // Bearer + " " + jwt
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+
+        let verified = jwt.verify(bearerToken, "brock");
+        req.userId = verified.id;
+
+        const checkUser = `SELECT *, rowid FROM user WHERE user.rowid = ${verified.id}`
+
+        database.all(checkUser, (err, checkedUser) => {
+            if(err){
+                return res.status(500).json({
+                    status: 500,
+                    message: "something went wrong, try again."
+                });
+            } else if (checkedUser.length === 0){
+                return res.status(403).json({ 
+                    status: 403,
+                    message: "requires permissions."
+                });
+            } else{
+                next();
+            };
+        });
+    } else{
+        return res.status(403).json({
+            status: 403,
+            message: "requires permissions."
+        });
+    };
+};
