e4c30e56791e209035183ba5bc98afef064100f0
RJ Bamrah <RJ@RJs-MacBook-Pro.local>
2020-01-31 12:05:52 -0800

    created containers OverviewBudgetEntryContainer

---
+++ frontend/src/Components/OverviewVisualComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class OverviewVisualComponent extends Component {
+  render() {
+    return (
+      <div>
+        Render graph
+        Render total
+      </div>
+    )
+  }
+}
+
+export default OverviewVisualComponent;
