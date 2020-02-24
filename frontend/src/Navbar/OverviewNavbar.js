d1a7e62f911bf083e63eac3d75769426379dbe74
ekcs0915 <ekcs0915@gmail.com>
2020-02-24 15:48:00 -0800

    took out Time submenu in the hamburger menu

--- frontend/src/Navbar/OverviewNavbar.js
+++ frontend/src/Navbar/OverviewNavbar.js
@@ -37,13 +37,9 @@ class OverviewNavbar extends Component {
 					<img src={BackButton} className="back-button" style={{height:35}} alt="BackButton SVG"/>
 				</a>
 					<nav className="side-nav">
-						<div><a onClick={this.openSubmenu} className="overview-navbar-link">Time</a>
-							<div className="sub-menu" style={{display: this.state.display}}>
-								<Link to = '/overview/daily' className="overview-navbar-link"> Daily </Link>
-								<Link to = '/overview/weekly' className="overview-navbar-link"> Weekly </Link>
-								<Link to = '/overview/monthly' className="overview-navbar-link"> Monthly </Link>
-							</div>
-						</div>
+						<Link to = '/overview/daily' className="overview-navbar-link"> Daily </Link>
+						<Link to = '/overview/weekly' className="overview-navbar-link"> Weekly </Link>
+						<Link to = '/overview/monthly' className="overview-navbar-link"> Monthly </Link>
 						<Link to = '/overview/profile' className="overview-navbar-link"> Profile </Link>
 						<Link to = '/' onClick={this.props.logout} className="overview-navbar-link overview-navbar-logout"> Sign Out </Link>
 					</nav>
