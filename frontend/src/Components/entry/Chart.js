e2cf90504454e11f4ecc91d560f94f3ca971c5a9
RJ Bamrah <RJ@RJs-MacBook-Pro.local>
2020-02-24 09:45:11 -0800

    added white font color to labels

--- frontend/src/Components/entry/Chart.js
+++ frontend/src/Components/entry/Chart.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {Pie} from 'react-chartjs-2';
+import {defaults} from 'react-chartjs-2';
+
+defaults.global.defaultFontColor = 'white';
 
 class Chart extends Component {
 
