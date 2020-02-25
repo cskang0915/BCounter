7dd9343f66a0b0120fd98f0f777845dd9560d36a
ekcs0915 <ekcs0915@gmail.com>
2020-02-24 17:26:27 -0800

    separated category title and the amount to make toggle-able

--- frontend/src/Containers/entry/OverviewBudgetEntryContainer.js
+++ frontend/src/Containers/entry/OverviewBudgetEntryContainer.js
@@ -2,28 +2,47 @@ import React, { Component } from 'react';
 import BudgetEntry from '../../Components/entry/BudgetEntry';
 
 class OverviewBudgetEntryContainer extends Component {
+	state = {
+		visibility: "visible"
+	}
+
+	componentDidMount() {
+		this.isVisible()
+	}
+
+	isVisible = () => {
+		if(this.props.data === "undefined"){
+			this.setState({
+				visibility: "hidden"
+			})
+		}
+	}
 
 	render() {
 		let budgetEntry
 		let sum = 0
 	  	if(typeof this.props.data === "undefined"){
-	  		budgetEntry = "there is no entries on this day"
+	  		budgetEntry = <div>no entries</div>
 	  	} else {
 	  		budgetEntry = this.props.data.map((entry) => {
 					sum = sum + parseFloat(entry.amount)
-		  		return <BudgetEntry data={entry} rowid={entry.rowid} time={this.props.time}/>
+		  		return (
+						<BudgetEntry data={entry} rowid={entry.rowid} time={this.props.time}/>
+						)
 		  	})
 	  	}
 	  	sum = sum.toFixed(2)
 
 	    return (
-	      <div className="OverviewBudgetEntryContainer-category-total">
-					<h2 className="OverviewBudgetEntryContainer-number">
-						<b className="OverviewBudgetEntryContainer-title">
-							{this.props.category} Total:&nbsp;${sum}
-						</b>
+	      <div>
+					{/* OverviewBudgetEntryContainer */}
+					<h2 style={{visibility: this.state.visibility}}>
+							{this.props.category}
 					</h2>
 	      	{budgetEntry}
+					<div style={{visibility: this.state.visibility}}>
+							Total:&nbsp;${sum}
+					</div>
 	      </div>
 	    )
 	}
