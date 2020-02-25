eec0bd81d2bb0e43494b6dfd057c02861587bd2b
ekcs0915 <ekcs0915@gmail.com>
2020-02-25 10:21:48 -0800

    made each BudgetEntryContainer individual toggles

--- frontend/src/Containers/entry/OverviewBudgetEntryContainer.js
+++ frontend/src/Containers/entry/OverviewBudgetEntryContainer.js
@@ -3,6 +3,22 @@ import BudgetEntry from '../../Components/entry/BudgetEntry';
 import './OverviewBudgetEntry.css'
 
 class OverviewBudgetEntryContainer extends Component {
+	state = {
+		display: "initial"
+	}
+
+	toggleCategory = () => {
+		if(this.state.display === "initial"){
+			this.setState({
+				display: "none"
+			})
+		}else if(this.state.display === "none"){
+			this.setState({
+				display: "initial"
+			})
+		}
+	}
+
 	render() {
 		let budgetEntry
 		let sum = 0
@@ -12,7 +28,7 @@ class OverviewBudgetEntryContainer extends Component {
 			budgetEntry = this.props.data.map((entry) => {
 				sum = sum + parseFloat(entry.amount)
 				return (
-					<BudgetEntry style={this.props.toggle} data={entry} rowid={entry.rowid} time={this.props.time}/>
+					<BudgetEntry style={this.state.display} data={entry} rowid={entry.rowid} time={this.props.time}/>
 					)
 			})
 		}
@@ -22,7 +38,7 @@ class OverviewBudgetEntryContainer extends Component {
 			<div>
 				{/* OverviewBudgetEntryContainer */}
 				<h2>
-					<button className="needsWantsSavings" onClick={this.props.toggleCategory}>
+					<button className="needsWantsSavings" onClick={this.toggleCategory}>
 						{this.props.category}
 					</button>
 				</h2>
